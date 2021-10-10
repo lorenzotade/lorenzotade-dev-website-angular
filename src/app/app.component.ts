d8cfc4adfeba7a4853285aaef53b0fe59ee4a9ce
lorenzotade <lorenzo.tade@gmail.com>
2021-10-10 21:14:03 +0200

    bugfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,5 @@
 import { Component } from '@angular/core';
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDH_fzz6C0f4kptb6OHgUB_0NoG45rOSbU",
-  authDomain: "lorenzotade-dev.firebaseapp.com",
-  projectId: "lorenzotade-dev",
-  storageBucket: "lorenzotade-dev.appspot.com",
-  messagingSenderId: "954541795844",
-  appId: "1:954541795844:web:a140bab5d840251819a351",
-  measurementId: "G-6NBKF9BF13"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,22 @@ import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDH_fzz6C0f4kptb6OHgUB_0NoG45rOSbU",
+  authDomain: "lorenzotade-dev.firebaseapp.com",
+  projectId: "lorenzotade-dev",
+  storageBucket: "lorenzotade-dev.appspot.com",
+  messagingSenderId: "954541795844",
+  appId: "1:954541795844:web:a140bab5d840251819a351",
+  measurementId: "G-6NBKF9BF13"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 @NgModule({
   declarations: [
     AppComponent,
